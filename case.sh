e826ef4ebb029e03f0a00f2ac1900a5ccfb88dbf
PREETI2096 <preetirg96@gmail.com>
2022-07-11 22:11:18 +0530

    added:case file

---
+++ case.sh
@@ -0,0 +1,17 @@
+read -p "enter week number:" week_num
+case $week_num in
+1)echo "SUNDAY"
+;;
+2)echo "MONDAY"
+;;
+3)echo "TUESDAY"
+;;
+4)echo "WEDNESDAy"
+;;
+5)echo "THURSDAY"
+;;
+6)echo "FRIDAY"
+;;
+*)echo "SATURDAY"
+esac
+
